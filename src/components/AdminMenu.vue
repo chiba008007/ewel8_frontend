b96d960b07db11d13ff78edbee0b968d2554f8cc
chiba008007 <chiba00807@gmail.com>
2024-06-13 00:56:43 +0900

    メニュー追加

--- src/components/AdminMenu.vue
+++ src/components/AdminMenu.vue
@@ -24,6 +24,7 @@ const pageClickMove = (pagename: string) => {
         density="compact"
         color="primary"
         class="ml-1 w-25"
+        @onClick="pageClickMove('addPertner')"
       ></ComponentButton>
       <ComponentButton
         text="受検者検索"
