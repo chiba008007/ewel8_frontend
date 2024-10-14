e072e1933996788182821e781975f0cc32d81062
chiba008007 <chiba00807@gmail.com>
2024-10-14 10:53:39 +0900

    api

--- src/user-common.ts
+++ src/user-common.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
   // APIのURI
-  //baseURL: "http://localhost:8000",
-  baseURL: "https://api.uh-oh.jp",
+  baseURL: "http://localhost:8000",
+  //baseURL: "https://api.uh-oh.jp",
   // リクエストヘッダ
   headers: {
     "Content-type": "application/json",
